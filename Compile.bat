e0312c5fea335ec480328781da203c673be3a50b
unknown <jonathanphillips@cb5107-04.labs.sce.carleton.ca>
2015-03-14 14:57:57 -0400

    Server.java 0.1 Implements message queue for incoming packets.

--- Compile.bat
+++ Compile.bat
@@ -2,4 +2,5 @@
 PATH = "C:\Program Files\Java\jdk1.7.0_67\bin"
 javac Vote.java
 javac Entry.java
+javac Server.java
 pause
--- Server.java
+++ Server.java
@@ -1,3 +1,66 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+import java.net.SocketException;
+import java.io.IOException;
+
 public class Server {
+	String[] candidates = null;
+	ConcurrentLinkedQueue<DatagramPacket> packetQueue;	//message queue
+	DatagramSocket socket;
 	
+	public Server(String[] candidates){
+		this.candidates = candidates;
+		this.packetQueue = new ConcurrentLinkedQueue<DatagramPacket>();
+		try{
+			this.socket = new DatagramSocket(5555);
+		} catch(SocketException e){	
+			e.printStackTrace();
+		}
+		this.main();
+	}
+	
+	private void main(){
+		Thread t1 = new Thread(new Runnable(){
+			public void run(){
+				while(true){
+					byte[] buf = new byte[128];
+					DatagramPacket receive = new DatagramPacket(buf,buf.length);
+					try{
+						socket.receive(receive);
+					} catch ( IOException e) {
+						e.printStackTrace();
+					}
+					System.out.println("Got packet from "+ receive.getAddress().getHostAddress());
+					packetQueue.add(receive);
+				}
+			}
+		});
+		Thread t2 = new Thread(new Runnable(){
+			public void run(){
+				while(true){
+				}
+			}
+		});
+
+		try{
+			t1.start();
+			t2.start();
+			t1.join();
+			t2.join();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean registerUser(){
+		return false;
+	}
+	
+	public boolean registerVote(){
+		return false;
+	}
 }
