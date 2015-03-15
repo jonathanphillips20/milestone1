1c6f5014ea602e980784c8defbba922c91b5547a
unknown <jonathanphillips@cb5107-04.labs.sce.carleton.ca>
2015-03-15 14:41:26 -0400

    Update compiler.

--- Compile.bat
+++ Compile.bat
@@ -1,16 +1,5 @@
 @echo off
 PATH = "C:\Program Files\Java\jdk1.7.0_45\bin"
-javac Vote.java -d %cd%\bin\
-javac Entry.java -d %cd%\bin\
-javac Server.java -d %cd%\bin\
-javac Client.java -d %cd%\bin\
-
-javac GUI.java -d %cd%\bin\
-javac GUI2.java -d %cd%\bin\
-javac GUI3.java -d %cd%\bin\
-javac GUI4.java -d %cd%\bin\
-javac DialogBox.java -d %cd%\bin\
-javac Controller2.java -d %cd%\bin\
-javac GUIMain.java -d %cd%\bin\
+javac *.java -d %cd%\bin\
 pause
 
